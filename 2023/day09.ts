5bb8dfbdbd86c88d1635013438497a76d895a99d
Adrian <107351903+6lr61@users.noreply.github.com>
2023-12-13 14:47:33 +0100

    refactor: infer string literal directly

--- 2023/day09.ts
+++ 2023/day09.ts
@@ -1,12 +1,4 @@
-type MakeReverseArray<
+type Reverse<
   S extends string,
-  A extends string[] = []
-> = S extends `${infer H}${infer T}` ? [...MakeReverseArray<T>, H] : A;
-
-type MakeString<A extends string[]> = A extends [infer H, ...infer T]
-  ? T extends string[]
-    ? `${H & string}${MakeString<T>}`
-    : ""
-  : "";
-
-type Reverse<S extends string> = MakeString<MakeReverseArray<S>>;
+  A extends string = ""
+> = S extends `${infer H}${infer T}` ? Reverse<T, `${H}${A}`> : A;
