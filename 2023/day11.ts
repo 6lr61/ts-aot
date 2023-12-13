34d9bb4cb7692b14f37aeea03c908d033b06b7b8
Adrian <107351903+6lr61@users.noreply.github.com>
2023-12-13 14:51:50 +0100

    refactor: do propper type guarding

--- 2023/day11.ts
+++ 2023/day11.ts
@@ -1,5 +1,9 @@
-type SantaListProtector<T> = {
-  readonly [key in keyof T]: T[key] extends Record<string, unknown> | unknown[]
-    ? SantaListProtector<T[key]>
-    : T[key];
-} & unknown;
+type SantaListProtector<T> = T extends Record<string, unknown> | unknown[]
+  ? {
+      readonly [key in keyof T]: T[key] extends
+        | Record<string, unknown>
+        | unknown[]
+        ? SantaListProtector<T[key]>
+        : T[key];
+    }
+  : T;
