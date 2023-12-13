3d242d60caf01694ffd3273cb459b7792b9e4285
Adrian <107351903+6lr61@users.noreply.github.com>
2023-12-13 11:26:10 +0100

    refactor: allow for any starting date

--- 2023/day13.ts
+++ 2023/day13.ts
@@ -1,7 +1,12 @@
-type DayCounter<
-  Start extends number,
+// Solution to starting from any day inspired by: https://github.com/TkDodo/aot-2023/blob/main/13.ts
+
+type Counter<
   Stop extends number,
   Acc extends number[] = [1]
 > = Stop extends Acc["length"]
   ? Acc[number]
-  : DayCounter<Start, Stop, [...Acc, [...Acc, 0]["length"]]>;
+  : Counter<Stop, [...Acc, [...Acc, 0]["length"]]>;
+
+type DayCounter<B extends number, E extends number> =
+  | B
+  | Exclude<Counter<E>, Counter<B>>;
